87ebec96003072d38c0b319b7d92603e25f749f9
Sherlock <sherlock@gmail.com>
2018-06-08 16:04:50 +0630

    Created Product Components via props

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <product-list-one v-bind:products="products"></product-list-one>
+    <product-list-two v-bind:products="products"></product-list-two>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import ProductListOne from './components/ProductListOne.vue';
+import ProductListTwo from './components/ProductListTwo.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components : {
+    'product-list-one' : ProductListOne,
+    'product-list-two' : ProductListTwo
+  },
+  data() {
+    return {
+      products: [
+                {name: 'Banana Skin', price: 20},
+                {name: 'Shiny Star', price: 40},
+                {name: 'Green Shells', price: 60},
+                {name: 'Red Shells', price: 80}
+            ]
+    }
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+    font-family: Ubuntu;
+    color: #555;
 }
 </style>
